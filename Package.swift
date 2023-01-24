56ce8fc95ae0d74dc66c5dff207a8252d9d94097
Sven A. Schmidt <sas@finestructure.co>
2023-01-24 11:56:57 +0100

    Lower tools-version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // Copyright Dave Verwer, Sven A. Schmidt, and other contributors.
